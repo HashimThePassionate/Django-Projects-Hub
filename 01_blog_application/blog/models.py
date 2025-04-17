4d605425ac659081ff80d9d8858af539aaa73e9b
Muhammad Hashim <hashiimtahir@gmail.com>
2025-04-17 05:40:32 +0500

    📝 Adding Many to one relationship

--- 01_blog_application/blog/models.py
+++ 01_blog_application/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -11,7 +12,8 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length==2, choices=Status,default=Status.Draft)
+    status = models.CharField(max_length=2, choices=Status,default=Status.Draft)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='blog_posts')
 
     class Meta:
         ordering = ['-publish']
