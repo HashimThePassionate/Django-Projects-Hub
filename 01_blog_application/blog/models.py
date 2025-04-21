fce6ca867efcb82af944b02dab462c4e2a076ba0
Muhammad Hashim <hashiimtahir@gmail.com>
2025-04-21 07:39:54 +0500

    📝 SEO-friendly URLs

--- 01_blog_application/blog/models.py
+++ 01_blog_application/blog/models.py
@@ -5,33 +5,48 @@ from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(status=Post.Status.Published)
-
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
 
 class Post(models.Model):
     class Status(models.TextChoices):
-        Draft = 'DF','Draft'
-        Published = 'PB','Published'
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
-    body =models.TextField()
+    slug = models.SlugField(max_length=250,unique_for_date='publish')
+    author = models.ForeignKey(  
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
+    body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2, choices=Status,default=Status.Draft)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='blog_posts')
-    objects = models.Manager()
-    published = PublishedManager()
-    class Meta:
+    created = models.DateTimeField(auto_now_add=True) 
+    updated = models.DateTimeField(auto_now=True)      
+    status = models.CharField( 
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
+
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+    class Meta: 
         ordering = ['-publish']
-        indexes = [
+        indexes = [ 
             models.Index(fields=['-publish']),
         ]
+    def get_absolute_url(self): 
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year, 
+                self.publish.month, 
+                self.publish.day,
+                self.slug 
+            ]
+        )
     
-    def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.id])
-    
-
     def __str__(self):
         return self.title
-    
