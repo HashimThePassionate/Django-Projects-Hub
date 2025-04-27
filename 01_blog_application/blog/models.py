5c4ed13a56f9da782217ed2df3e41e9359c0eda2
Muhammad Hashim <hashiimtahir@gmail.com>
2025-04-28 03:47:26 +0500

    📝 Implementing tagging

--- 01_blog_application/blog/models.py
+++ 01_blog_application/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 from django.urls import reverse
-
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -34,6 +34,7 @@ class Post(models.Model):
 
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # Our custom manager.
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
