49d72530ce36698cbe61026a9305990e8e1f4dbf
Muhammad Hashim <hashiimtahir@gmail.com>
2025-04-29 02:31:04 +0500

    📝 Shiftig to postgresql db

--- 01_blog_application/mysite/settings.py
+++ 01_blog_application/mysite/settings.py
@@ -85,14 +85,13 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+ 'default': {
+ 'ENGINE': 'django.db.backends.postgresql',
+ 'NAME': os.getenv('DB_NAME'),
+ 'USER': os.getenv('DB_USER'),
+ 'PASSWORD': os.getenv('DB_PASSWORD'),
+ 'HOST': os.getenv('DB_HOST'),
+ }
 }
 
 
