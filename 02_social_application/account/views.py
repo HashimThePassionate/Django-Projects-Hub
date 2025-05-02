7a8853d88b31e54304a4431d750d66921cbb335a
Muhammad Hashim <hashiimtahir@gmail.com>
2025-05-03 04:17:21 +0500

    📝 Login View

--- 02_social_application/account/views.py
+++ 02_social_application/account/views.py
@@ -8,20 +8,16 @@ def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            cd = form.cleaned_data
+            fm = form.cleaned_data
             user = authenticate(
-                request,
-                username=cd['username'],
-                password=cd['password']
-            )
+                request, username=fm['username'], password=fm['password'])
             if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated successfully')
-                else:
-                    return HttpResponse('Disabled account')
+                login(request,user)
+                return HttpResponse('Authenticated successful')
             else:
-                return HttpResponse('Invalid login')
+                return HttpResponse('Disabled account')
+        else:
+            return HttpResponse('Invalid login')
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
