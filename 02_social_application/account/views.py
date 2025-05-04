d36087b070952582b34eb074111f8521ba6fd3e5
Muhammad Hashim <hashiimtahir@gmail.com>
2025-05-04 10:34:05 +0500

    📝 Register New User

--- 02_social_application/account/views.py
+++ 02_social_application/account/views.py
@@ -1,16 +1,30 @@
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+
 
 @login_required
 def dashboard(request):
- return render(
- request,
- 'account/dashboard.html',
- {'section': 'dashboard'}
- )
+    return render(
+        request,
+        'account/dashboard.html',
+        {'section': 'dashboard'}
+    )
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        new_user = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': new_user})
 
 
 def user_login(request):
@@ -21,7 +35,7 @@ def user_login(request):
             user = authenticate(
                 request, username=fm['username'], password=fm['password'])
             if user is not None:
-                login(request,user)
+                login(request, user)
                 return HttpResponse('Authenticated successful')
             else:
                 return HttpResponse('Disabled account')
