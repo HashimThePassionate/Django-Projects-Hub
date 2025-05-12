15731c067ec43718b396f347b9f25b40d556e6b0
Muhammad Hashim <hashiimtahir@gmail.com>
2025-05-12 11:53:29 +0500

    📝 Detail Image  View

--- 02_social_application/images/models.py
+++ 02_social_application/images/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
+
 
 class Image(models.Model):
     user = models.ForeignKey(
@@ -11,7 +13,8 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     desription = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
+    users_like = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
 
     class Meta:
         indexes = [
@@ -24,6 +27,8 @@ class Image(models.Model):
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
 
     def __str__(self):
         return self.title
